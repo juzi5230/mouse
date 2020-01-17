1e04fcf7295158105f501b003760b45af8700613
xjcheng4 <xjcheng4@iflytek.com>
2020-01-17 16:55:21 +0800

    主页基础布局

--- lib/tabBar.dart
+++ lib/tabBar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 class Choice {
-  const Choice({ this.title, this.icon, this.index });
+  const Choice({ this.title, this.icon, this.index});
   final String title;
   final IconData icon;
   final int index;
@@ -14,6 +14,24 @@ class Choice {
   const Choice(title: 'WALK', icon: Icons.directions_walk, index: 6),
 ];
 
+class CardList {
+  const CardList({ this.assetSrc, this.title, this.desc});
+  final String assetSrc;
+  final String title;
+  final String desc;
+}
+
+const List<CardList> cardLists = const <CardList>[
+  const CardList(assetSrc: '', title: '', desc: ''),
+  const CardList(assetSrc: '', title: '', desc: ''),
+  const CardList(assetSrc: '', title: '', desc: ''),
+  const CardList(assetSrc: '', title: '', desc: ''),
+  const CardList(assetSrc: '', title: '', desc: ''),
+  const CardList(assetSrc: '', title: '', desc: ''),
+  const CardList(assetSrc: '', title: '', desc: ''),
+];
+
+const entries = ['1', '2', '3','1', '2', '3', '1', '2', '3','1', '2', '3','1', '2', '3'];
 class ChoiceCard extends StatelessWidget {
   const ChoiceCard({ Key key, this.choice }) : super(key: key);
 
@@ -21,20 +39,65 @@ class ChoiceCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final TextStyle textStyle = Theme.of(context).textTheme.display1;
+    // final TextStyle textStyle = Theme.of(context).textTheme.display1;
     return new Card(
       color: Colors.white,
       child: new Center(
-        child: 
-        new Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            new Icon(choice.icon, size: 128.0, color: textStyle.color),
-            new Text(choice.title + '${choice.index}', style: textStyle),
-          ],
-        ),
+        child: new CardContent(choice: choice,)
       ),
     );
+  }
+}
+
+class CardContent extends StatelessWidget {
+  const CardContent({Key key, this.choice}) : super(key: key);
+  final Choice choice;
+  @override
+  Widget build(BuildContext context) {
+    if(choice.index == 1) {
+      return ListView.separated(
+        padding: const EdgeInsets.all(8),
+        itemCount: cardLists.length,
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+        itemBuilder: (BuildContext context, int index) {
+          return Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  new Expanded(
+                    flex: 2,
+                    child: Container(
+                      // child: Text('内容展示21',),
+                      child: Image(
+                        image: AssetImage('images/nalan.jpg'),
+                        fit: BoxFit.fill
+                      ),
+                      height: 100,
+                    ), 
+                  ),
+                  new Expanded(
+                    flex: 3,
+                    child: Container(
+                      child: Text(
+                        '描述性文字121',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                        ),
+                      ),
+                    ),
+                  ), 
+                ]
+              ),
+            ]
+          );
+        }
+      );
+    } else {
+      return new Container(
+        child:Text('12222')
+      );
+    }
   }
 }
